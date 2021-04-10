698537860a44d7cfd70f15173eda38400b6addf9
frank <keanuf@gmx.de>
2021-04-10 16:29:33 +0200

    Kennenlernen des Requests modul mit html parser an einem Beispiel abfrage bei ebay klainanzeige

---
+++ 15_minuten_requests/main.py
@@ -0,0 +1,15 @@
+from requests_html import HTMLSession
+
+
+## URLS
+# Requests https://2.python-requests.org/en/master/user/quickstart/#redirection-and-history
+# Requests html https://requests-html.kennethreitz.org/
+
+headers = {'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0'}
+
+session = HTMLSession()
+
+url = "https://www.ebay-kleinanzeigen.de/s-suchanfrage.html?keywords=honda+nc+700"#&categoryId=&locationStr=91074 Herzogenaurach&locationId=6203&radius=0&sortingField=SORTING_DATE&adType=&posterType=&pageNum=1&action=find&maxPrice=&minPrice="
+
+er = session.get(url, headers=headers)
+print(er.text)
