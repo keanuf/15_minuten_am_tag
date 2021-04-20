b0f67a93a562faa7c63a36d6128c2c932cb32fd9
frank <keanuf@gmx.de>
2021-04-20 19:46:44 +0200

    Abfrage nach Preis, Beschreibung, und Link als Funktion eingebaut

--- 15_minuten_requests/main.py
+++ 15_minuten_requests/main.py
@@ -9,14 +9,23 @@ headers = {'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/
 
 session = HTMLSession()
 
+kleinanzeigen_url = "https://www.ebay-kleinanzeigen.de"
 url = "https://www.ebay-kleinanzeigen.de/s-suchanfrage.html?keywords=honda+nc+700"#&categoryId=&locationStr=91074 Herzogenaurach&locationId=6203&radius=0&sortingField=SORTING_DATE&adType=&posterType=&pageNum=1&action=find&maxPrice=&minPrice="
 
 er = session.get(url, headers=headers)
 artikel = er.html.find('article')
 
-for a in artikel:
-    print(a.attrs['data-href'])
-    #print(a.html)
-    ## nach css class noch nix gefunden mal schauen ob ich es schaffe ;)
-    prize = a.find(".aditem-main--middle--price") # find nicht vom html objekt dann klappt es auch
-    print(prize[0].text)
+def get_items_of_link(artikel):
+    data = []
+    for a in artikel:
+        link = kleinanzeigen_url + a.attrs['data-href']
+        prize = a.find(".aditem-main--middle--price") # find nicht vom html objekt dann klappt es auch
+        description = a.find(".aditem-main--middle--description")
+        data.append({'link':link, 'prize':prize[0].text, 'description':description[0].text})
+    return data
+
+print(get_items_of_link(artikel)[0])
+
+### links der folgenden Seiten abrufen noch ein Todo
+for a in er.html:
+    print(a)
